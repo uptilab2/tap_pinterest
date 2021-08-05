2e1e2d5448dd81cce297cdb226a2fe25a9ffc44c
Hugo Moracchini <hugo4400@gmail.com>
2021-08-05 16:15:22 +0200

    Rework sync to support 30 day segments for reports

--- tap_pinterest/sync.py
+++ tap_pinterest/sync.py
@@ -1,9 +1,10 @@
 import singer
 import backoff
 from singer import metrics, metadata, Transformer, utils, UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 LOGGER = singer.get_logger()
+BASE_URL = 'https://api.pinterest.com/ads/v3'
 
 
 def write_schema(catalog, stream_name):
@@ -77,7 +78,15 @@ def process_records(catalog, stream_name, records, time_extracted,
         return max_bookmark_value, counter.value
 
 
-def get_advertiser_ids(client, url, owner_user_id):
+def get_advertiser_ids(client, url, owner_user_id=None):
+
+    if not owner_user_id:
+        res = client.get(f'{BASE_URL}/users/me/')
+        if res['data']:
+            owner_user_id = res['data']['id']
+        else:
+            return {''}
+
     res = client.get(url=url, endpoint='advertisers', params=dict(owner_user_id=owner_user_id, include_acl=True))
     return [adveriser['id'] for adveriser in res['data']]
 
@@ -85,7 +94,7 @@ def get_advertiser_ids(client, url, owner_user_id):
 # Sync a specific parent or child endpoint.
 def sync_endpoint(client, catalog, state, start_date, stream_name, path, endpoint_config, parent_id=None):
 
-    url = f'https://api.pinterest.com/ads/v3/{path}'
+    url = f'{BASE_URL}/{path}'
 
     # Two types of endpoints exist, each with their own logic.
     if (endpoint_config.get('async_report', False)):
@@ -132,12 +141,8 @@ def sync_rest_endpoint(client, catalog, state, url, stream_name, start_date, end
                     child_last_datetime = get_bookmark(state, stream_name, start_date)
                     child_max_bookmarks[child_stream_name] = child_last_datetime
 
-    if endpoint_config.get('owner_user_id'):
-        path = 'https://api.pinterest.com/ads/v3/advertisers'
-        advertiser_ids = endpoint_config['advertiser_ids'] or get_advertiser_ids(client, path, endpoint_config['owner_user_id'])
-    else:
-        # At least loop once, with no id.
-        advertiser_ids = {''}
+    path = '{BASE_URL}/advertisers'
+    advertiser_ids = endpoint_config['advertiser_ids'] or get_advertiser_ids(client, path, endpoint_config.get('owner_user_id'))
 
     total_records = 0
 
@@ -240,9 +245,7 @@ def sync_async_endpoint(client, catalog, state, url, stream_name, start_date, en
     """
 
     # Request params
-    # start_date and end_date are already defined in the endpoints dict
-    # NOTE: Documentation specifies start_date and end_date cannot be more than 30 days appart
-    # TODO: Handle this.
+    # start_date and end_date are already defined in the endpoints dict    
     body = endpoint_config.get('params')
 
     # Get the latest bookmark for the stream and set the last_datetime
@@ -250,17 +253,24 @@ def sync_async_endpoint(client, catalog, state, url, stream_name, start_date, en
     max_bookmark_value = last_datetime
     LOGGER.info(f'{stream_name}: bookmark last_datetime = {max_bookmark_value}')
 
-    # Bookmark query field is always 'start_date' in our case, but lets keep it variable.
-    bookmark_query_field = endpoint_config.get('bookmark_query_field', 'start_date')
-    if bookmark_query_field:
-        body[bookmark_query_field] = datetime.strftime(last_datetime, "%Y-%m-%d")
+    # NOTE: Documentation specifies start_date and end_date cannot be more than 30 days appart.
+    segments = {}
+    now = datetime.date().today()
 
-    if endpoint_config.get('owner_user_id'):
-        path = 'https://api.pinterest.com/ads/v3/advertisers'
-        advertiser_ids = endpoint_config['advertiser_ids'] or get_advertiser_ids(client, path, endpoint_config['owner_user_id'])
+    segment_start = last_datetime
+    segment_end = segment_start + datetime.timedelta(days=30)
+    if segment_end > now:
+        segments.add((segment_start, now))
     else:
-        # At least loop once, with no id.
-        advertiser_ids = {''}
+        while segment_end <= now:
+            segments.add((segment_start, segment_end))
+            segment_start = segment_end + timedelta(days=1)
+            segment_end = segment_end + timedelta(days=30)
+        if segment_end > now:
+            segments.add((segment_start, now))
+
+    path = '{BASE_URL}/advertisers'
+    advertiser_ids = endpoint_config['advertiser_ids'] or get_advertiser_ids(client, path, endpoint_config.get('owner_user_id'))
 
     total_records = 0
 
@@ -270,51 +280,63 @@ def sync_async_endpoint(client, catalog, state, url, stream_name, start_date, en
         # Set advertiser ID, if present in string.
         url = original_url.format(advertiser_id=advertiser_id)
 
-        # Create request to generate report
-        LOGGER.info(f'URL for {stream_name}: {url} -> body: {body.items()}')
-        res = client.post(url=url, endpoint=stream_name, data=body)
+        for start, end in segments:
 
-        # If the report generates instantly
-        if res['data'].get('report_status') == 'FINISHED':
-            token = res['data'].get('token')
-        else:
-            token = retry_report(client, 'post', url, stream_name, data=body)
+            start = start.strftime("%Y-%m-%d")
+            end = end.strftime("%Y-%m-%d")
 
-        # GET the report data using the token
-        LOGGER.info(f'Getting report with token: {token}')
-        res = client.get(url=url, endpoint=stream_name, params=dict(token=token))
+            LOGGER.info(f' -- Looking up data for segment : {start} --TO-> {end}')
 
-        # Here we do the same retry.
-        # Normally this should never be used as we wait in the first step, but lets be safe.
-        if res['data'].get('report_status') == 'FINISHED':
-            report_url = res['data'].get('url')
-        else:
-            report_url = retry_report(client, 'get', url, stream_name, params=dict(token=token))
+            body.update({
+                'start_date': start,
+                'end_date': end
+            })
 
-        # Now that we have the report, we need to dowload the link to the file.
-        data = client.download_report(report_url)
-        total_records = 0
+            # Create request to generate report
+            LOGGER.info(f'URL for {stream_name}: {url} -> body: {body.items()}')
+            res = client.post(url=url, endpoint=stream_name, data=body)
 
-        if not data:
-            LOGGER.info(f' -- No data for report at : {report_url}')
+            # If the report generates instantly
+            if res['data'].get('report_status') == 'FINISHED':
+                token = res['data'].get('token')
+            else:
+                token = retry_report(client, 'post', url, stream_name, data=body)
 
-        for line in data:
-            # time_extracted: datetime when the data was extracted from the API
-            time_extracted = utils.now()
+            # GET the report data using the token
+            LOGGER.info(f'Getting report with token: {token}')
+            res = client.get(url=url, endpoint=stream_name, params=dict(token=token))
 
-            # Process records and get the max_bookmark_value and record_count for the set of records
-            max_bookmark_value, record_count = process_records(
-                catalog=catalog,
-                stream_name=stream_name,
-                records=line,
-                time_extracted=time_extracted,
-                bookmark_field=endpoint_config.get('bookmark_field'),
-                max_bookmark_value=max_bookmark_value,
-                last_datetime=last_datetime,
-                parent=endpoint_config.get('parent'),
-                parent_id=parent_id)
-            LOGGER.info(f'{stream_name}: Synced report. Total records processed: {record_count}')
-            total_records = total_records + record_count
+            # Here we do the same retry.
+            # Normally this should never be used as we wait in the first step, but lets be safe.
+            if res['data'].get('report_status') == 'FINISHED':
+                report_url = res['data'].get('url')
+            else:
+                report_url = retry_report(client, 'get', url, stream_name, params=dict(token=token))
+
+            # Now that we have the report, we need to dowload the link to the file.
+            data = client.download_report(report_url)
+            total_records = 0
+
+            if not data:
+                LOGGER.info(f' -- No data for report at : {report_url}')
+
+            for line in data:
+                # time_extracted: datetime when the data was extracted from the API
+                time_extracted = utils.now()
+
+                # Process records and get the max_bookmark_value and record_count for the set of records
+                max_bookmark_value, record_count = process_records(
+                    catalog=catalog,
+                    stream_name=stream_name,
+                    records=line,
+                    time_extracted=time_extracted,
+                    bookmark_field=endpoint_config.get('bookmark_field'),
+                    max_bookmark_value=max_bookmark_value,
+                    last_datetime=start,
+                    parent=endpoint_config.get('parent'),
+                    parent_id=parent_id)
+                LOGGER.info(f'{stream_name}: Synced report. Total records processed: {record_count}')
+                total_records = total_records + record_count
 
         return total_records, max_bookmark_value
 
@@ -377,7 +399,6 @@ def should_sync_stream(selected_streams, last_stream, stream_name):
 def sync(client, config, catalog, state):
     date = datetime.strptime(config['start_date'], "%Y-%m-%dT%H:%M:%SZ")
     date_string = datetime.strftime(date, "%Y-%m-%d")
-    now = datetime.strftime(utils.now(), "%Y-%m-%d")
 
     selected_streams = get_selected_streams(catalog)
     LOGGER.info(f'selected_streams: {selected_streams}')
@@ -427,12 +448,10 @@ def sync(client, config, catalog, state):
             'advertiser_ids': config.get('advertiser_ids'),
             'owner_user_id': config.get('owner_user_id'),
             'params': {
-                'end_date': now,
                 'granularity': 'DAY',  # This returns one record per day, no need to iterate on days like some other taps
                 'level': 'ADVERTISER',
             },
             'bookmark_field': 'DATE',
-            'bookmark_query_field': 'start_date',
             'id_fields': ['ADVERTISER_ID'],
             'async_report': True
         },
@@ -442,13 +461,11 @@ def sync(client, config, catalog, state):
             'advertiser_ids': config.get('advertiser_ids'),
             'owner_user_id': config.get('owner_user_id'),
             'params': {
-                'end_date': now,
                 'granularity': 'DAY',  # This returns one record per day, no need to iterate on days like some other taps
                 'level': 'CAMPAIGN',
                 'entity_fields': 'CAMPAIGN_MANAGED_STATUS,CAMPAIGN_NAME,CAMPAIGN_STATUS'
             },
             'bookmark_field': 'DATE',
-            'bookmark_query_field': 'start_date',
             'id_fields': ['CAMPAIGN_ID'],
             'async_report': True
         },
@@ -458,13 +475,11 @@ def sync(client, config, catalog, state):
             'advertiser_ids': config.get('advertiser_ids'),
             'owner_user_id': config.get('owner_user_id'),
             'params': {
-                'end_date': now,
                 'granularity': 'DAY',  # This returns one record per day, no need to iterate on days like some other taps
                 'level': 'AD_GROUP',
                 'entity_fields': 'AD_GROUP_STATUS,AD_GROUP_NAME'
             },
             'bookmark_field': 'DATE',
-            'bookmark_query_field': 'start_date',
             'id_fields': ['CAMPAIGN_ID'],
             'async_report': True
         }
