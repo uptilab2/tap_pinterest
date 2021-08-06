4fdfc5100ca2d68bc612efee89fbf1150f225e2f
Hugo Moracchini <hugo4400@gmail.com>
2021-08-06 12:37:36 +0200

    Oh sweet jesus

--- tap_pinterest/sync.py
+++ tap_pinterest/sync.py
@@ -518,7 +518,7 @@ def sync(client, config, catalog, state):
             # Write parent bookmarks
             if bookmark_field:
                 if type(max_bookmark_value) is datetime:
-                    max_bookmark_value = max_bookmark_value.strdtime("%Y-%m-%dT%H:%M:%SZ")
+                    max_bookmark_value = max_bookmark_value.strptime("%Y-%m-%dT%H:%M:%SZ")
                 write_bookmark(state, stream_name, max_bookmark_value)
 
             update_currently_syncing(state, None)
