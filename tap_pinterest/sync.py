4c4869f81a51561ded1e5bf63365bf29e922b341
Hugo Moracchini <hugo4400@gmail.com>
2021-09-27 15:19:02 +0200

    Custom reports don't have bookmarks, how did this even work ??

--- tap_pinterest/sync.py
+++ tap_pinterest/sync.py
@@ -67,7 +67,7 @@ def process_records(catalog, stream_name, records, time_extracted,
             if parent_id and parent:
                 record[parent + '_id'] = parent_id
 
-            bookmark_dttm = datetime.strptime(record[bookmark_field], '%Y-%m-%d')
+            bookmark_dttm = datetime.strptime(record[bookmark_field], '%Y-%m-%d') or datetime.now()
 
             if isinstance(max_bookmark_value, str):
                 max_bookmark_value = datetime.strptime(max_bookmark_value, "%Y-%m-%dT%H:%M:%SZ")
